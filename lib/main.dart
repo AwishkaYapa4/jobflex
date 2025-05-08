11c47255c15546d33d95817bdd6dbdbc11e9ed32
Awishka Yapa <168206870+AwishkaYapa4@users.noreply.github.com>
2025-05-08 10:38:13 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:jobflex/profile/user_pro.dart';
 import 'package:jobflex/footer_pages/more.dart';
 import 'package:jobflex/startpages/loging.dart';
+<<<<<<< HEAD
 import 'package:jobflex/supportive_pages/setting_page.dart';
+=======
+import 'package:jobflex/screan/home.dart';
+>>>>>>> a0dbe0323d5b796632ff6eedde42f54dd8fb6079
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +19,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+<<<<<<< HEAD
       home: const SettingPage(),
+=======
+      home: const HomePage(),
+>>>>>>> a0dbe0323d5b796632ff6eedde42f54dd8fb6079
       routes: {
         '/userProfile': (context) => const UserPro(),
         '/more': (context) => const More(), // Example route
