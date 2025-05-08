a0dbe0323d5b796632ff6eedde42f54dd8fb6079
dilhara2 <hansanidilhara0202@gmail.com>
2025-05-08 10:34:11 +0530

    Home page start

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:jobflex/profile/user_pro.dart';
 import 'package:jobflex/footer_pages/more.dart';
 import 'package:jobflex/startpages/loging.dart';
+import 'package:jobflex/screan/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const More(),
+      home: const HomePage(),
       routes: {
         '/userProfile': (context) => const UserPro(),
         '/more': (context) => const More(), // Example route
---
+++ lib/screan/home.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final jobCategories = [
+      {'title': 'Event Crew', 'image': 'assets/images/event_crew.png'},
+      {'title': 'Super market', 'image': 'assets/images/super_market.png'},
+      {'title': 'Supportive team', 'image': 'assets/images/supportive_team.png'},
+      {'title': 'Marketing', 'image': 'assets/images/marketing.png'},
+      {'title': 'Pet care', 'image': 'assets/images/pet_care.png'},
+      {'title': 'Health Care', 'image': 'assets/images/health_care.png'},
+    ];
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFF5F9FF),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Your Job\nNeeds',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),
+              ),
+              const SizedBox(height: 8),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Hi [User’s First Name], Find Your Next Flex Job Today!',
+                    style: TextStyle(fontSize: 14, color: Colors.black54),
+                  ),
+                  Icon(Icons.search, color: Colors.black54),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: GridView.builder(
+                  itemCount: jobCategories.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 12,
+                    mainAxisSpacing: 12,
+                    childAspectRatio: 0.9,
+                  ),
+                  itemBuilder: (context, index) {
+                    final job = jobCategories[index];
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            job['image']!,
+                            height: 80,
+                            fit: BoxFit.contain,
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            job['title']!,
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.blue,
+        backgroundColor: Color(0xFF1F2A44),
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: 'Chat'),
+          BottomNavigationBarItem(icon: Icon(Icons.work), label: 'Jobs'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.notifications), label: 'Notifications'),
+          BottomNavigationBarItem(icon: Icon(Icons.more_horiz), label: 'More'),
+        ],
+      ),
+    );
+  }
+}
+
