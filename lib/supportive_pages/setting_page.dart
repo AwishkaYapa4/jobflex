5cafb2d392e1d3fe23bb1002970fb14fb3e29413
Awishka Yapa <168206870+AwishkaYapa4@users.noreply.github.com>
2025-05-11 09:37:01 +0530

    Update setting_page.dart

--- lib/supportive_pages/setting_page.dart
+++ lib/supportive_pages/setting_page.dart
@@ -42,6 +42,7 @@ class _SettingPageState extends State<SettingPage> {
               children: [
                 const SizedBox(height: 20),
                 SettingsItem(icon: Icons.language, title: 'Languages'),
+                SettingsItem(icon: Icons.settings, title: 'Account Settings'),
               ],
             ),
           ),
