669847b5ba98a305b5ad90113a13d547a4487db4
Awishka Yapa <168206870+AwishkaYapa4@users.noreply.github.com>
2025-05-14 14:23:53 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,10 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: SettingPage());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: const More());
   }
 }
