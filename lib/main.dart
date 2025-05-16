266d1ec7d150026b5d50341dff581fd3a40ba0cc
Milanka08 <milankamuthuni5@gmail.com>
2025-05-16 16:35:34 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-=======
-
->>>>>>> 7831e6f30bcf473af3f4bd59d658ad314dddc243
 import 'package:jobflex/footer_pages/more.dart';
 import 'package:jobflex/profile/user_pro.dart';
 import 'package:jobflex/startpages/loging.dart';
@@ -10,15 +6,8 @@ import 'package:jobflex/supportive_pages/setting_page.dart';
 import 'package:jobflex/screan/home.dart';
 import 'package:jobflex/profile/promotor_profile.dart';
 import 'package:jobflex/widget/footer.dart';
-<<<<<<< HEAD
-import 'package:jobflex/supportive_pages/setting_page.dart';
 import 'package:jobflex/supportive_pages/invite_friend.dart';
-=======
-
-import 'package:jobflex/supportive_pages/setting_page.dart';
 import 'package:jobflex/widget/constants.dart';
-import 'package:jobflex/widget/footer.dart';
->>>>>>> 7831e6f30bcf473af3f4bd59d658ad314dddc243
 
 void main() {
   runApp(const MyApp());
