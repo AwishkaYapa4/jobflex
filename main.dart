d05a272ee30dd2a2c2795aa751a2bee292b2e0c6
Milanka08 <milankamuthuni5@gmail.com>
2025-05-16 16:03:22 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import 'package:flutter/material.dart';
 import 'package:jobflex/profile/promotor_profile.dart';
 import 'package:jobflex/widget/setting_tile.dart';
@@ -20,3 +21,24 @@ class JobflexApp extends StatelessWidget {
     );
   }
 }
+=======
+import 'package:flutter/material.dart';
+import 'package:jobflex/notification_page.dart';
+
+void main() {
+  runApp(const JobflexApp());
+}
+
+class JobflexApp extends StatelessWidget {
+  const JobflexApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Jobflex',
+      debugShowCheckedModeBanner: false,
+      home: NotificationPage(),
+    );
+  }
+}
+>>>>>>> 1a793af967b3671963b83245b7e714e1b54a5287
