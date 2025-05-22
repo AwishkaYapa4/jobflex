85525f0bf922f54f9a3cc0389bc19d2f7d4e5f38
paukawya <136374184+paukawya@users.noreply.github.com>
2025-05-23 02:39:13 +0530

    update

--- lib/profile/user_admin.dart
+++ lib/profile/user_admin.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+<<<<<<< HEAD
 void main() {
   runApp(
     const MaterialApp(
@@ -33,10 +34,15 @@ class UserAdminScreen extends StatelessWidget {
       'contact': '8967',
     },
   ];
+=======
+class UserAdmin extends StatelessWidget {
+  const UserAdmin({Key? key}) : super(key: key);
+>>>>>>> ffe6ddff33183fad672d4e46781433a32016a08d
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+<<<<<<< HEAD
       backgroundColor: const Color(0xFFE7EBF0),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: const Color(0xFF1D2D5D),
@@ -223,6 +229,99 @@ class UserAdminScreen extends StatelessWidget {
             ),
             const SizedBox(height: 10),
           ],
+=======
+      appBar: AppBar(title: const Text('User/Admin'), centerTitle: true),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Table(
+            border: TableBorder.all(),
+            columnWidths: const {
+              0: FixedColumnWidth(50),
+              1: FixedColumnWidth(100),
+              2: FixedColumnWidth(150),
+              3: FixedColumnWidth(80),
+              4: FixedColumnWidth(50),
+              5: FixedColumnWidth(50),
+            },
+            defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+            children: [
+              TableRow(
+                decoration: const BoxDecoration(color: Colors.grey),
+                children: [
+                  TableCell(child: Center(child: Text('ID'))),
+                  TableCell(child: Center(child: Text('Job title'))),
+                  TableCell(child: Center(child: Text('Email'))),
+                  TableCell(child: Center(child: Text('Contact'))),
+                  TableCell(child: Center(child: Text('Edit'))),
+                  TableCell(child: Center(child: Text('Delete'))),
+                ],
+              ),
+              TableRow(
+                children: [
+                  TableCell(child: Center(child: Text('1'))),
+                  TableCell(child: Center(child: Text('Max Mendes'))),
+                  TableCell(child: Center(child: Text('max@gmail.com'))),
+                  TableCell(child: Center(child: Text('3567'))),
+                  TableCell(child: Center(child: Icon(Icons.edit))),
+                  TableCell(
+                    child: Center(child: Icon(Icons.delete, color: Colors.red)),
+                  ),
+                ],
+              ),
+              TableRow(
+                children: [
+                  TableCell(child: Center(child: Text('2'))),
+                  TableCell(child: Center(child: Text('Mark Hugo'))),
+                  TableCell(child: Center(child: Text('mark@gmail.com'))),
+                  TableCell(child: Center(child: Text('6437'))),
+                  TableCell(child: Center(child: Icon(Icons.edit))),
+                  TableCell(
+                    child: Center(child: Icon(Icons.delete, color: Colors.red)),
+                  ),
+                ],
+              ),
+              TableRow(
+                children: [
+                  TableCell(child: Center(child: Text('3'))),
+                  TableCell(child: Center(child: Text('Smith'))),
+                  TableCell(child: Center(child: Text('smith@gmail.com'))),
+                  TableCell(child: Center(child: Text('8654'))),
+                  TableCell(child: Center(child: Icon(Icons.edit))),
+                  TableCell(
+                    child: Center(child: Icon(Icons.delete, color: Colors.red)),
+                  ),
+                ],
+              ),
+              TableRow(
+                children: [
+                  TableCell(child: Center(child: Text('4'))),
+                  TableCell(child: Center(child: Text('Kendrick'))),
+                  TableCell(child: Center(child: Text('kendrick@gmail.com'))),
+                  TableCell(child: Center(child: Text('8967'))),
+                  TableCell(child: Center(child: Icon(Icons.edit))),
+                  TableCell(
+                    child: Center(child: Icon(Icons.delete, color: Colors.red)),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomSheet: Container(
+        height: 300,
+        decoration: const BoxDecoration(
+          color: Color(0xFF1A2A4B),
+          borderRadius: BorderRadius.vertical(top: Radius.circular(0)),
+        ),
+        child: Center(
+          child: Image.asset(
+            'Icons/Logo.png',
+            // Replace with your image path
+            height: 180,
+          ),
+>>>>>>> ffe6ddff33183fad672d4e46781433a32016a08d
         ),
       ),
     );
