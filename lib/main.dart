51c4c1383dad8df02a38f6533264e9b7ff300bca
Awishka Yapa <168206870+AwishkaYapa4@users.noreply.github.com>
2025-05-24 16:25:07 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,14 @@
 import 'package:flutter/material.dart';
-
 import 'package:jobflex/chat/chat.dart';
 import 'package:jobflex/chat/chat_box.dart';
 import 'package:jobflex/chat/new_chat.dart';
 import 'package:jobflex/chat/chat_search.dart';
-
 import 'package:jobflex/footer_pages/more.dart';
 import 'package:jobflex/payment/payment.dart';
 import 'package:jobflex/profile/promo_admin.dart';
 import 'package:jobflex/profile/user_admin.dart';
 import 'package:jobflex/profile/user_pro.dart';
 import 'package:jobflex/startpages/loging.dart';
-import 'package:jobflex/startpages/welcome_page.dart';
 import 'package:jobflex/supportive_pages/setting_page.dart';
 import 'package:jobflex/screan/home.dart';
 import 'package:jobflex/profile/promotor_profile.dart';
@@ -25,14 +22,14 @@ void main() {
   runApp(const MyApp());
 }
 
-class NewChat extends StatelessWidget {
-  const NewChat({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const NewChat(),
+      home: const PromotorProfile(),
     );
   }
 }
