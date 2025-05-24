d40e5d31fd77be680dec0e1cb95f8ae98c367538
Awishka Yapa <168206870+AwishkaYapa4@users.noreply.github.com>
2025-05-24 14:46:35 +0530

    Update footer.dart

--- lib/widget/footer.dart
+++ lib/widget/footer.dart
@@ -2,33 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:jobflex/widget/constants.dart';
 
 class Footer extends StatelessWidget {
-  const Footer({super.key});
+  const Footer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return NavigationBar(
-      backgroundColor: JPrimaryColor,
-      destinations: const [
-        NavigationDestination(
-          icon: Icon(Icons.chat_bubble_outline, color: Colors.white),
-          label: 'Chat',
+    return Container(
+      decoration: BoxDecoration(
+        color: JPrimaryColor,
+        boxShadow: [
+          BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(.1)),
+        ],
+      ),
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              _buildFooterItem(Icons.chat_bubble_outline, 'Chat'),
+              _buildFooterItem(Icons.work_outline, 'Jobs'),
+              _buildFooterItem(Icons.home_outlined, 'Home'),
+              _buildFooterItem(Icons.notifications_none, 'Notifications'),
+              _buildFooterItem(Icons.more_horiz, 'More'),
+            ],
+          ),
         ),
-        NavigationDestination(
-          icon: Icon(Icons.work_outline, color: Colors.white),
-          label: 'Jobs',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.home_outlined, color: Colors.white),
-          label: 'Home',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.notifications_none, color: Colors.white),
-          label: 'Notifications',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.more_horiz, color: Colors.white),
-          label: 'More',
-        ),
+      ),
+    );
+  }
+
+  Widget _buildFooterItem(IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(icon, color: Colors.white, size: 30), // Increased icon size
+        Text(label, style: const TextStyle(color: Colors.white)),
       ],
     );
   }
