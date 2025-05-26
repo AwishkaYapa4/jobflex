9b7b24be9dda49761edf6ae292de90ab05ba9098
Milanka08 <milankamuthuni5@gmail.com>
2025-05-26 21:24:03 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ import 'package:jobflex/payment/payment.dart';
 import 'package:jobflex/profile/promo_admin.dart';
 import 'package:jobflex/profile/user_admin.dart';
 import 'package:jobflex/profile/user_pro.dart';
-import 'package:jobflex/screan/Choice.dart';
 import 'package:jobflex/screan/categories.dart';
 import 'package:jobflex/startpages/loging.dart';
 import 'package:jobflex/supportive_pages/setting_page.dart';
@@ -23,7 +22,7 @@ import 'package:jobflex/supportive_pages/help_center.dart';
 import 'package:jobflex/models/setting.dart';
 import 'package:jobflex/widget/setting_tile.dart';
 import 'package:jobflex/widget/footer.dart';
-
+import 'package:jobflex/screan/Choice.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -35,9 +34,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const ChoiceScreen(), // Set HomePage as the initial page
-      /*initialRoute: '/',
-     routes: {
+     /* initialRoute: '/',
+      routes: {
         '/': (context) => const HomePage(), // Set HomePage as the initial page
         '/more': (context) => const More(),
         '/settingpage': (context) => const SettingPage(),
@@ -48,10 +46,13 @@ class MyApp extends StatelessWidget {
         '/chatbox': (context) => const ChatBox(),
         '/newchat': (context) => const NewChat(),
         '/chatsearch': (context) => const ChatSearch(),
+
         '/signup': (context) => const SignUpPage(),
+
         '/promotorprofile': (context) => const PromotorProfile(),
         '/useradmin': (context) => const UserAdmin(),
-      }*/
+      },*/
+      home: const ChoiceScreen(), // Set ChoiceScreen as the initial page
     );
   }
 }
