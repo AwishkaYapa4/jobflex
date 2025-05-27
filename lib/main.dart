a0b92c0484db2c8a81f6b02bbf1e851cb1cbcdd6
Awishka Yapa <168206870+AwishkaYapa4@users.noreply.github.com>
2025-05-27 11:36:33 +0530

    logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:jobflex/chat/chat.dart';
+import 'package:jobflex/chat/chat_box.dart';
+import 'package:jobflex/chat/new_chat.dart';
+import 'package:jobflex/chat/chat_search.dart';
 import 'package:jobflex/footer_pages/more.dart';
-<<<<<<< Updated upstream
-=======
 import 'package:jobflex/payment/payment.dart';
 import 'package:jobflex/profile/promo_admin.dart';
 import 'package:jobflex/profile/user_admin.dart';
->>>>>>> Stashed changes
 import 'package:jobflex/profile/user_pro.dart';
 import 'package:jobflex/screan/Choice.dart';
 import 'package:jobflex/screan/categories.dart';
@@ -13,37 +14,27 @@ import 'package:jobflex/startpages/loging.dart';
 import 'package:jobflex/supportive_pages/setting_page.dart';
 import 'package:jobflex/screan/home.dart';
 import 'package:jobflex/profile/promotor_profile.dart';
-<<<<<<< Updated upstream
-import 'package:jobflex/widget/footer.dart';
-import 'package:jobflex/supportive_pages/setting_page.dart';
-import 'package:jobflex/payment/pay_succ.dart';
-
-=======
 import 'package:jobflex/supportive_pages/invite_friend.dart';
 import 'package:jobflex/startpages/sign_up.dart';
 import 'package:jobflex/supportive_pages/help_center.dart';
 import 'package:jobflex/models/setting.dart';
-import 'package:jobflex/widget/footer.dart';
->>>>>>> Stashed changes
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< Updated upstream
-    return MaterialApp(debugShowCheckedModeBanner: false, home: CategoriesScreen());
-=======
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      /* initialRoute: '/',
+      initialRoute: '/',
       routes: {
-        '/': (context) => const HomePage(), // Set HomePage as the initial page
+        '/':
+            (context) =>
+                const HomePage(), // Set ChoiceScreen as the initial page
         '/more': (context) => const More(),
         '/settingpage': (context) => const SettingPage(),
         '/payment': (context) => PaymentScreen(),
@@ -53,16 +44,15 @@ class MyApp extends StatelessWidget {
         '/chatbox': (context) => const ChatBox(),
         '/newchat': (context) => const NewChat(),
         '/chatsearch': (context) => const ChatSearch(),
-        //  '/categories': (context) => const CategoriesPage(),
-        //  '/login': (context) => const LoginPage(),
+        '/categories': (context) => CategoriesScreen(),
+        //'/login': (context) => const LoginPage(),
         '/signup': (context) => const SignUpPage(),
-        //    '/userprofile': (context) => const UserProfile(),
+        //'/userprofile': (context) => const UserProfile(),
         '/promotorprofile': (context) => const PromotorProfile(),
         '/useradmin': (context) => const UserAdmin(),
         //'/promoadmin': (context) => const PromoAdmin(),
-      },*/
-      home: const ChoiceScreen(), // Set ChoiceScreen as the initial page
+        '/home': (context) => const HomePage(),
+      },
     );
->>>>>>> Stashed changes
   }
 }
