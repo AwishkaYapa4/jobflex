e8072e47bc48ad9b04a37f249ca703edfe1bf658
T.M.Gayantha Nadeemal <gayanthanadeemal@gmail.com>
2025-05-29 23:03:47 +0530

    Update categories.dart

--- lib/screan/categories.dart
+++ lib/screan/categories.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:jobflex/widget/footer.dart';
-import 'home.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Job {
   final String title;
